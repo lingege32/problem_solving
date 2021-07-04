a7d6f1b430720f577712aa01667dbb6de7d5e787
lingege32 <lingege32@gmail.com>
2021-07-04 14:04:07 +0800

    update other's solution

--- src/leetcode/find_min_height_trees.rs
+++ src/leetcode/find_min_height_trees.rs
@@ -1,3 +1,76 @@
+// Other's Solution
+// use std::collections::HashMap;
+// impl Solution {
+//     pub fn find_min_height_trees(n: i32, edges: Vec<Vec<i32>>) -> Vec<i32> {
+//         if edges.len() == 0 {
+//             return vec![0];
+//         }
+//         let mut degree = vec![0; n as usize];
+//         let mut adj: HashMap<i32, Vec<i32>> = HashMap::new();
+//         for edge in edges {
+//             if let Some(a) = adj.get_mut(&edge[0]) {
+//                 (*a).push(edge[1]);
+//             } else {
+//                 adj.insert(edge[0], vec![edge[1]]);
+//             }
+//             if let Some(a) = adj.get_mut(&edge[1]) {
+//                 (*a).push(edge[0]);
+//             } else {
+//                 adj.insert(edge[1], vec![edge[0]]);
+//             }
+//             degree[edge[0] as usize] += 1;
+//             degree[edge[1] as usize] += 1;
+//         }
+
+//         let mut queue = vec![];
+//         for i in 0..n {
+//             if degree[i as usize] == 1 {
+//                 queue.push(i);
+//             }
+//         }
+
+//         let mut v = n;
+//         while v > 2 {
+//             v -= queue.len() as i32;
+//             for _i in 0..queue.len() {
+//                 // let t = *queue.first().unwrap() as i32;
+//                 let t = queue.remove(0) as i32;
+//                 for &j in adj.get(&t).unwrap() {
+//                     degree[j as usize] -= 1;
+//                     if degree[j as usize] == 1 {
+//                         queue.push(j);
+//                     }
+//                 }
+//             }
+//         }
+//         return queue;
+//         // let mut result = vec![];
+//         // println!("debug queue {:?}", queue);
+//         // while !queue.is_empty() {
+//         //     result.push(queue.remove(0) as i32);
+//         // }
+//         // println!("debug {:?}", result);
+//         // return result;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// my Solution
+
 use std::{collections::HashSet, hash::Hasher};
 struct Solution();
 struct Graph {
