36f376c216b5f3ae06ad5f6a923aa757dbff4b66
lingege32 <lingege32@gmail.com>
2021-07-19 20:16:22 +0800

    enhance memory used

--- src/leetcode/path_sum_ii.rs
+++ src/leetcode/path_sum_ii.rs
@@ -21,35 +21,31 @@ use std::cell::RefCell;
 use std::rc::Rc;
 impl Solution {
     pub fn path_sum(root: Option<Rc<RefCell<TreeNode>>>, target_sum: i32) -> Vec<Vec<i32>> {
-        let mut res = Self::inner(root, target_sum);
+        let mut res = Vec::new();
+        Self::inner(root, target_sum, &mut res);
         for v in res.iter_mut() {
             v.reverse();
         }
         res
     }
-    fn inner(root: Option<Rc<RefCell<TreeNode>>>, target_sum: i32) -> Vec<Vec<i32>> {
+    fn inner(root: Option<Rc<RefCell<TreeNode>>>, target_sum: i32, res_vec: &mut Vec<Vec<i32>>)  {
         if let Some(r) = root {
             fn is_leaf(node: &Rc<RefCell<TreeNode>>) -> bool {
                 node.borrow().left.is_none() && node.borrow().right.is_none()
             }
             if is_leaf(&r) {
                 if r.borrow().val == target_sum {
-                    vec![vec![target_sum]]
-                } else {
-                    Vec::new()
+                    res_vec.push(vec![target_sum]);
                 }
             } else {
                 let val = r.borrow().val;
-                let mut left_v = Self::inner(r.borrow().left.clone(), target_sum - val);
-                let mut right_v = Self::inner(r.borrow().right.clone(), target_sum - val);
-                left_v.append(&mut right_v);
-                for v in left_v.iter_mut() {
+                let cur_size = res_vec.len();
+                Self::inner(r.borrow().left.clone(), target_sum - val, res_vec);
+                Self::inner(r.borrow().right.clone(), target_sum - val, res_vec);
+                for v in &mut res_vec[cur_size..] {
                     v.push(val);
                 }
-                left_v
             }
-        } else {
-            Vec::new()
         }
     }
 }
