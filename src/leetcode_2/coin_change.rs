31db87cf24508171fdfaeee2b4d06fca6c908e5f
lingege32 <lingege32@gmail.com>
2021-11-19 00:08:20 +0800

    Enhance 322. Coin Change

--- src/leetcode_2/coin_change.rs
+++ src/leetcode_2/coin_change.rs
@@ -38,14 +38,17 @@ struct Solution {}
 // DP!
 impl Solution {
     pub fn coin_change(coins: Vec<i32>, amount: i32) -> i32 {
+        let mut coins = coins;
+        coins.sort_unstable();
         let mut dp = vec![i32::MAX; amount as usize +1];
         dp[0] = 0;
         for i in 1..=amount {
             let mut current_min = i32::MAX;
             for &coin in coins.iter() {
-                if i - coin >= 0 {
-                    current_min = std::cmp::min(current_min, dp[(i - coin) as usize]);
+                if i < coin {
+                    break;
                 }
+                current_min = std::cmp::min(current_min, dp[(i - coin) as usize]);
             }
 
             if current_min != i32::MAX {
