45a87637e3e961e8ba59b5b779f4caa380ad41a1
lingege32 <lingege32@gmail.com>
2021-12-14 20:55:25 +0800

    Finish 938. Range Sum of BST

--- src/leetcode_3/range_sum_bst.rs
+++ src/leetcode_3/range_sum_bst.rs
@@ -8,6 +8,7 @@ pub struct TreeNode {
 
 impl TreeNode {
     #[inline]
+    #[allow(dead_code)]
     pub fn new(val: i32) -> Self {
         TreeNode {
             val,
@@ -51,5 +52,35 @@ impl Solution {
                 ans
             }
         }
+    }
+}
+
+// use more memory
+struct Solution2 {}
+impl Solution2 {
+    #[allow(dead_code)]
+    pub fn range_sum_bst(root: Option<Rc<RefCell<TreeNode>>>, low: i32, high: i32) -> i32 {
+        let mut stack = Vec::new();
+        let mut ans = 0;
+        let mut node = root;
+        while node.is_some() || !stack.is_empty() {
+            match node {
+                Some(n) => {
+                    stack.push(Some(n.clone()));
+                    node = n.borrow_mut().left.take();
+                }
+                None => {
+                    let last = stack.pop().unwrap().unwrap();
+                    let v = last.borrow().val;
+                    if v > high {
+                        break;
+                    } else if v >= low {
+                        ans += v;
+                    }
+                    node = last.borrow_mut().right.take();
+                }
+            }
+        }
+        ans
     }
 }
