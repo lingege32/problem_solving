7369cc2789929b047548e0bef17c8982fd9b3981
lingege32 <lingege32@gmail.com>
2023-02-03 05:26:51 -0800

    merge

--- leetcode_5/src/leetcode_5.rs
+++ leetcode_5/src/leetcode_5.rs
@@ -2,10 +2,7 @@ mod guess_number;
 mod roman_to_int;
 mod calculate;
 mod sum_subarray_mins;
-<<<<<<< HEAD
 mod job_scheduling;
-=======
 mod best_team_score;
 mod gcd_of_strings;
 mod is_alien_sorted;
->>>>>>> 4581a0bfcbff1704f6565310e14c2f462ff4ca17
