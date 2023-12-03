f789d866638d631739ac26da7407ccbb5cbca85a
lingege32 <lingege32@gmail.com>
2023-12-03 19:35:04 +0800

    finish minimum_added_coins

--- leetcode_contest/src/leetcode_contest/minimum_added_coins.rs
+++ leetcode_contest/src/leetcode_contest/minimum_added_coins.rs
@@ -1,49 +1,25 @@
 struct Solution;
 impl Solution {
     #[allow(dead_code)]
-    pub fn minimum_added_coins(coins: Vec<i32>, target: i32) -> i32 {
-        let ori = coins.len();
-        let mut coins = coins;
-        coins.sort_unstable();
-        for t in 1..=target {
-            Self::fill_coin(&mut coins, t);
+    pub fn minimum_added_coins(mut arr: Vec<i32>, target: i32) -> i32 {
+      arr.sort_unstable();
+      let (mut seen_until, mut res) = (1u64, 0);
+      for v in arr {
+        while v as u64 > seen_until {
+          seen_until += seen_until;
+          res += 1;
         }
-        (coins.len() - ori) as i32
+        seen_until += v as u64;
+      }
+  
+      while seen_until < target as u64 + 1 {
+        seen_until += seen_until;
+        res += 1;
+      }
+  
+      return res;
     }
-
-    fn fill_coin(coins: &mut Vec<i32>, target: i32) {
-        if !Self::has_solution(coins, target) {
-            let idx = Self::upper_bound(coins, target);
-            coins.insert(idx, target);
-        }
-    }
-
-    fn has_solution(coins: &[i32], t: i32) -> bool {
-        if t == 0 {
-            return true;
-        }
-        let idx = Self::upper_bound(coins, t);
-        if idx == 0 {
-            return false;
-        }
-        let v = coins[idx - 1];
-        return Self::has_solution(&coins[0..idx - 1], t - v);
-    }
-
-    fn upper_bound(coins: &[i32], target: i32) -> usize {
-        let (mut left, mut right) = (0usize, coins.len());
-        while left < right {
-            let mid = left + (right - left) / 2;
-            if coins[mid] <= target {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-        left
-    }
-}
-
+  }
 #[cfg(test)]
 mod test_super {
     use super::*;
