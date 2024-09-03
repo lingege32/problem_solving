ffa2d46d3c2ec2ba7034ad0e563c5f9672908aa6
lingege32 <lingege32@gmail.com>
2024-09-02 23:03:30 -0700

    Finish countOfPairs with my method and optimize

--- leetcode_cpp/contest/0811/countOfPairs.cpp
+++ leetcode_cpp/contest/0811/countOfPairs.cpp
@@ -1,31 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-class Solution {
-public:
-  int countOfPairs(vector<int> &nums) {
-    auto first = nums[0];
-    vector<int> dp(first + 1, 1);
-    int limit = first;
-    for (auto it = nums.begin() + 1; it != nums.end(); ++it) {
-      auto sec = *it;
-      std::vector<int> sec_dp(sec + 1, 0);
-      for (int i = 0; i <= sec; ++i) {
-        for (int k = 0; k <= i && k <= limit; ++k) {
-          if (limit - k >= sec - i) {
-            sec_dp[i] += dp[k];
-            sec_dp[i] %= 1000000007;
-          }
-        }
-      }
 
-      std::swap(limit, sec);
-      std::swap(sec_dp, dp);
+class Solution {
+  public:
+    int countOfPairs(vector<int> &nums) {
+        auto first = nums[0];
+        vector<int> dp(first + 1, 1);
+        int prev = first;
+        for (auto it = nums.begin() + 1; it != nums.end(); ++it) {
+            auto cur = *it;
+            std::vector<int> sec_dp(cur + 1, 0);
+            int cnt = 0;
+            int prev_k = 0;
+            for (int i = 0; i <= cur; ++i) {
+                if (prev_k <= std::min(i, i - (cur - prev))) {
+                    cnt = (cnt + dp[prev_k]) % 1000000007;
+                    prev_k++;
+                }
+                sec_dp[i] = cnt;
+            }
+
+            std::swap(prev, cur);
+            std::swap(sec_dp, dp);
+        }
+        int ret = 0;
+        for (auto d : dp) {
+            ret += d;
+            ret %= 1000000007;
+        }
+        return ret;
     }
-    int ret = 0;
-    for (auto d : dp) {
-      ret += d;
-      ret %= 1000000007;
-    }
-    return ret;
-  }
 };
