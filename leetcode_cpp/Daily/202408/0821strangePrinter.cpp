d812de9da8e31730765409236a9ccfe9df6eadcf
lingege32 <lingege32@gmail.com>
2024-09-10 08:26:33 -0700

    Optimize

--- leetcode_cpp/Daily/202408/0821strangePrinter.cpp
+++ leetcode_cpp/Daily/202408/0821strangePrinter.cpp
@@ -3,59 +3,44 @@ using namespace std;
 
 class Solution {
   public:
-    int n = 1;
-    array<vector<int>, 26> alphabet;
+    template <typename T, size_t N>
+    struct TwoDimArray {
+        std::array<T, N * N> data;
 
-    void shorten(string& s) {
-        int sz = s.size();
-        // Use 2 pointer to remove successive duplicates
-        char cur;
-        for (int l = 0, r = 1; r < sz; r++) {
-            cur = s[r];
-            while (l < r && s[l] == cur) {
-                l++;
-            }
-            if (r < sz && s[l] != cur) {
-                alphabet[cur - 'a'].push_back(n);
-                s[n++] = cur;
-                l = r;
-            }
+        T* operator[](size_t k) { return &data[k * N]; }
+    };
+
+    static int strangePrinter(string s) {
+        int n = s.length();
+        vector<int> prevs(n, -1);
+        std::array<int, 26> char_prev;
+        char_prev.fill(-1);
+        for (int i = 0; i < n; i++) {
+            int key = s[i] - 'a';
+            prevs[i] = char_prev[key];
+            char_prev[key] = i;
         }
-        s.resize(n);  // s is shortened
-        //    cout<<s<<endl;
-    }
 
-    int strangePrinter(string& s) {
-        shorten(s);
-        //    cout<<n<<", shorten s="<<s<<endl;
-        std::vector<std::vector<int>> dp(101, std::vector<int>(101, 0));
-        for (int i = n - 1; i >= 0; i--) {     // backward
-            dp[i][i] = 1;                      // base case;
-            for (int j = i + 1; j < n; j++) {  // forward
-                int x = dp[i][j - 1];
-                if (s[i] == s[j]) {
-                    dp[i][j] = x;
-                    continue;
-                }
-                x++;
-                if (j - i <= 5) {
-                    for (int k = i + 1; k < j - 1; k++) {
-                        if (s[k] == s[j]) {
-                            x = min(x, dp[i][k - 1] + dp[k][j - 1]);
-                            k++;  // no successive duplicates
-                        }
-                    }
+        TwoDimArray<int, 100> min_turns;
+        min_turns[0][0] = 1;
+        for (int end = 1; end < n; end++) {
+            min_turns[end][end] = 1;
+            for (int start = end - 1; start >= 0; start--) {
+                if (prevs[end] == end - 1) {
+                    min_turns[start][end] = min_turns[start][end - 1];
+                } else if (prevs[end] < start) {
+                    min_turns[start][end] = min_turns[start][end - 1] + 1;
                 } else {
-                    auto& K = alphabet[s[j] - 'a'];
-                    int l = lower_bound(K.begin(), K.end(), i + 1) - K.begin();
-                    //    cout<<j<<"->"<<s[j]<<" l="<<l<<", K[l]="<<K[l]<<endl;
-                    for (size_t k = l; k < K.size() && K[k] < j - 1; k++) {
-                        x = min(x, dp[i][K[k] - 1] + dp[K[k]][j - 1]);
+                    min_turns[start][end] = min_turns[start][end - 1] + 1;
+                    int prev = prevs[end];
+                    while (prev >= start) {
+                        min_turns[start][end] = min(min_turns[start][end],
+                                                    min_turns[start][prev] + min_turns[prev + 1][end - 1]);
+                        prev = prevs[prev];
                     }
                 }
-                dp[i][j] = x;
             }
         }
-        return dp[0][n - 1];
+        return min_turns[0][n - 1];
     }
 };
