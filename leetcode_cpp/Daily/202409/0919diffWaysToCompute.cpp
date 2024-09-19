b28a515b47c3dd0c48b338ea2e827a6cbe66183f
lingege32 <lingege32@gmail.com>
2024-09-19 03:23:00 -0700

    [Daily][0919] 241. Different Ways to Add Parentheses

---
+++ leetcode_cpp/Daily/202409/0919diffWaysToCompute.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+  public:
+    vector<int> diffWaysToCompute(string expression) {
+        vector<int> res;
+        for (size_t i = 0; i < expression.size(); ++i) {
+            char oper = expression[i];
+            if (oper == '+' || oper == '-' || oper == '*') {
+                vector<int> s1 = diffWaysToCompute(expression.substr(0, i));
+                vector<int> s2 = diffWaysToCompute(expression.substr(i + 1));
+                for (int a : s1) {
+                    for (int b : s2) {
+                        if (oper == '+') {
+                            res.push_back(a + b);
+                        } else if (oper == '-') {
+                            res.push_back(a - b);
+                        } else if (oper == '*') {
+                            res.push_back(a * b);
+                        }
+                    }
+                }
+            }
+        }
+        if (res.empty()) {
+            res.push_back(stoi(expression));
+        }
+        return res;
+    }
+};
--- leetcode_cpp/Daily/README.md
+++ leetcode_cpp/Daily/README.md
@@ -49,3 +49,4 @@
 [0916 Medium - 539. Minimum Time Difference](https://leetcode.com/problems/minimum-time-difference/description/?envType=daily-question&envId=2024-09-16)
 [0917 Easy - 884. Uncommon Words from Two Sentences](https://leetcode.com/problems/uncommon-words-from-two-sentences/description/?envType=daily-question&envId=2024-09-17)
 [0918 Medium - 179. Largest Number](https://leetcode.com/problems/largest-number/description/?envType=daily-question&envId=2024-09-18)
+[0919 Medium - 241. Different Ways to Add Parentheses](https://leetcode.com/problems/different-ways-to-add-parentheses/description/?envType=daily-question&envId=2024-09-19)
