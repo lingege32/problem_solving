38a2541da0b352a7e485a135448f3e662402c918
lingege32 <lingege32@gmail.com>
2024-09-28 00:00:29 -0700

    Add description

--- leetcode_cpp/Daily/202407/0708findTheWinner.cpp
+++ leetcode_cpp/Daily/202407/0708findTheWinner.cpp
@@ -17,3 +17,17 @@ class Solution {
         return winner + 1;
     }
 };
+
+/*
+n=4, k=2
+
+1. 0 1 2 3 // index (2+2) % s is winner
+2. 0 2 3 -> 2 3 0  // index (0+2) % 3 is winner
+3. 2 0 -> 0 2 // index (0 + k)%n is winner
+4. 0 // index 0 is winner
+
+* after step 1, remove the index 1 from the vector and start from index 2
+* if we rotate the index 2 to the first, we can say that it start from index 0;
+* To think reversely, if we want to know who is the winner index of the array with n elements, we can take the winner index x with n-1 elements and shift right k steps.
+
+*/
