6a2f79a044b976fac435ae1bf3401affdb137e90
lingege32 <lingege32@gmail.com>
2024-10-02 01:58:56 -0700

    Optimize the code

--- leetcode_cpp/Leetcode75/SlidingWindow/longestOnes.cpp
+++ leetcode_cpp/Leetcode75/SlidingWindow/longestOnes.cpp
@@ -8,32 +8,24 @@ auto init = []() {
 }();
 
 class Solution {
-    static bool canPlace(const vector<int>& nums, int m, int k) {
-        auto how_many_one = nums[m - 1];
-        for (int n = m; n < static_cast<int>(nums.size()); ++n) {
-            if (how_many_one + k >= m) {
-                return true;
-            }
-            how_many_one = nums[n] - nums[n - m];
-        }
-        return how_many_one + k >= m;
-    }
-
-  public:
+public:
     static int longestOnes(vector<int>& nums, int k) {
-        for (size_t i = 1; i < nums.size(); ++i) {
-            nums[i] += nums[i - 1];
-        }
-        int l = 1;
-        int r = nums.size() + 1;
-        while (l < r) {
-            int m = l + ((r - l) >> 1);
-            if (canPlace(nums, m, k)) {
-                l = m + 1;
-            } else {
-                r = m;
+        int ans = 0;
+        int zero = 0;
+        int l = 0;
+        for(int r = 0; r < nums.size(); ++r) {
+            if(nums[r] == 0) {
+                zero++;
             }
+            if(zero > k) {
+                if(nums[l] == 0) {
+                    zero--;
+                }
+                l++;
+            } else {
+                ans = max(ans, r - l + 1);
+            }  
         }
-        return l - 1;
+        return ans;
     }
 };
