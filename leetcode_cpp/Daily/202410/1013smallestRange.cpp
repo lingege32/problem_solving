7bdbed130034b94cc5ae79750dbfee1f7a606cec
lingege32 <lingege32@gmail.com>
2024-10-13 07:52:11 -0700

    [Daily][1013][rewrite] 632. Smallest Range Covering Elements from K Lists

--- leetcode_cpp/Daily/202410/1013smallestRange.cpp
+++ leetcode_cpp/Daily/202410/1013smallestRange.cpp
@@ -12,31 +12,31 @@ auto init = []() {
 class Solution {
   public:
     static vector<int> smallestRange(vector<vector<int>>& nums) {
-        auto k = nums.size();
-        auto comp = [](const auto& a, const auto& b) { return a.first > b.first; };
-        vector<int> indexes(k, 0);
-        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(comp)> pq(comp);
-        int mx = INT_MIN;
-        for (size_t i = 0; i < k; i++) {
+        auto n = nums.size();
+        vector<int> idx(n, 0);
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+
+        int max = std::numeric_limits<int>::min();
+        for (size_t i = 0; i < n; ++i) {
+            max = std::max(max, nums[i][0]);
             pq.emplace(nums[i][0], i);
-            mx = max(mx, nums[i][0]);
         }
-        vector<int> ans(2);
-        ans[0] = pq.top().first;
-        ans[1] = mx;
-        while (pq.size() == k) {
-            pair<int, int> ele = pq.top();
+        vector<int> ret = {pq.top().first, max};
+
+        while (pq.size() == n) {
+            auto [v, i] = pq.top();
             pq.pop();
-            if (mx - ele.first < ans[1] - ans[0]) {
-                ans[0] = ele.first;
-                ans[1] = mx;
+            if (max - v < ret[1] - ret[0]) {
+                ret = {v, max};
             }
-            if (static_cast<size_t>(indexes[ele.second]) < (nums[ele.second].size() - 1)) {
-                indexes[ele.second]++;
-                pq.emplace(nums[ele.second][indexes[ele.second]], ele.second);
-                mx = max(mx, nums[ele.second][indexes[ele.second]]);
+            ;
+            if ((++idx[i]) == static_cast<int>(nums[i].size())) {
+                break;
             }
+            pq.emplace(nums[i][idx[i]], i);
+            max = std::max(max, nums[i][idx[i]]);
         }
-        return ans;
+        return ret;
     }
 };
