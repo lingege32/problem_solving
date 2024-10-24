22eddc23a43ed22a3697c579f687bc7747663e7c
lingege32 <lingege32@gmail.com>
2024-10-24 01:41:11 -0700

    [Daily][1024] 951. Flip Equivalent Binary Trees

---
+++ leetcode_cpp/Daily/202410/1024flipEquiv.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode():
+      val(0),
+      left(nullptr),
+      right(nullptr) {}
+
+    explicit TreeNode(int x):
+      val(x),
+      left(nullptr),
+      right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right):
+      val(x),
+      left(left),
+      right(right) {}
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+  public:
+    static bool flipEquiv(TreeNode *root1, TreeNode *root2) {
+        if (root1 == nullptr && root2 == nullptr) {
+            return true;
+        }
+        if (root1 == nullptr || root2 == nullptr) {
+            return false;
+        }
+
+        if (root1->val != root2->val) {
+            return false;
+        }
+
+        return (Solution::flipEquiv(root1->left, root2->left) && Solution::flipEquiv(root1->right, root2->right)) ||
+               (Solution::flipEquiv(root1->left, root2->right) && Solution::flipEquiv(root1->right, root2->left));
+    }
+};
--- leetcode_cpp/Daily/README.md
+++ leetcode_cpp/Daily/README.md
@@ -119,3 +119,4 @@
 [1021 Medium - 1593. Split a String Into the Max Number of Unique Substrings](https://leetcode.com/problems/split-a-string-into-the-max-number-of-unique-substrings/description/?envType=daily-question&envId=2024-10-21)
 [1022 Medium - 2583. Kth Largest Sum in a Binary Tree](https://leetcode.com/problems/kth-largest-sum-in-a-binary-tree/description/?envType=daily-question&envId=2024-10-22)
 [1023 Medium - 2641. Cousins in Binary Tree II](https://leetcode.com/problems/cousins-in-binary-tree-ii/description/?envType=daily-question&envId=2024-10-23replaceValueInTree)
+[1024 Medium - 951. Flip Equivalent Binary Trees](https://leetcode.com/problems/flip-equivalent-binary-trees/description/?envType=daily-question&envId=2024-10-24)
