b98a4ecd68f2cca61fee080546e87e73dc468ddc
lingege32 <lingege32@gmail.com>
2024-10-25 04:50:27 -0700

    [Codewars] 6kyu - Is a number prime?

---
+++ leetcode_cpp/CodeWars/isPrime.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+
+bool isPrime(int num) {
+    if (num < 2) {
+        return false;
+    }
+    for (int i = 2; i * i <= num; ++i) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
