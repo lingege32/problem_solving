041d730bb9408c7d3f4f1aae015945fc12d0a445
lingege32 <lingege32@gmail.com>
2024-12-20 20:47:54 -0800

    [Daily][1221] 2872. Maximum Number of K-Divisible Components

---
+++ leetcode_cpp/Daily/202412/1221maxKDivisibleComponents.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+
+typedef long long int ll;
+const int N = 3e4 + 5;
+
+vector<vector<int>> g(N);
+vector<ll> val(N);
+int result;
+int K;
+
+int MaxComponents(int src, int par) {
+    ll leftOver = val[src];
+
+    for (auto i : g[src]) {
+        if (i == par) {
+            continue;
+        }
+        leftOver += MaxComponents(i, src);
+    }
+    if (leftOver % K == 0) {
+        result++;
+        leftOver = 0;
+    }
+    return leftOver;
+}
+
+class Solution {
+  public:
+    static int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
+        result = 0;
+        K = k;
+        for (int j = 0; j < n; j++) {
+            g[j].clear();
+            val[j] = values[j];
+        }
+        for (auto e : edges) {
+            g[e[0]].push_back(e[1]);
+            g[e[1]].push_back(e[0]);
+        }
+
+        ll leftOver = MaxComponents(0, -1);
+        if (leftOver != 0) {
+            result++;
+        }
+
+        return result;
+    }
+};
--- leetcode_cpp/Daily/README.md
+++ leetcode_cpp/Daily/README.md
@@ -181,3 +181,4 @@
 [1218 Easy - 1475. Final Prices With a Special Discount in a Shop](https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/description/?envType=daily-question&envId=2024-12-18)
 [1219 Medium - 769. Max Chunks To Make Sorted](https://leetcode.com/problems/max-chunks-to-make-sorted/description/?envType=daily-question&envId=2024-12-19)
 [1220 Medium - 2415. Reverse Odd Levels of Binary Tree](https://leetcode.com/problems/reverse-odd-levels-of-binary-tree/description/?envType=daily-question&envId=2024-12-20)
+[1221 Hard - 2872. Maximum Number of K-Divisible Components](https://leetcode.com/problems/maximum-number-of-k-divisible-components/description/?envType=daily-question&envId=2024-12-21)
