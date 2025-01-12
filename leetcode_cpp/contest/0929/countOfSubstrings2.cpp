859bd4a182c1ab167a0bf559fe5fbf4549e2105a
lingege32 <lingege32@gmail.com>
2025-01-12 04:46:06 -0800

    update

--- leetcode_cpp/contest/0929/countOfSubstrings2.cpp
+++ leetcode_cpp/contest/0929/countOfSubstrings2.cpp
@@ -11,13 +11,8 @@ class Solution {
   public:
     static bool isVowel(char ch) { return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'); }
 
-    static long long countOfSubstrings(const string& word, int k) {
+    static vector<int> createNextConsonant(const std::string& word) {
         int n = word.size();
-        unordered_map<char, int> vowels;
-        int consonantCount = 0;
-        long long result = 0;
-
-        // Precompute next consonant positions
         vector<int> nextConsonant(n);
         int lastConsonant = n;
         for (int i = n - 1; i >= 0; i--) {
@@ -26,7 +21,22 @@ class Solution {
                 lastConsonant = i;
             }
         }
+        return nextConsonant;
+    }
 
+    static long long countOfSubstrings(const string& word, int k) {
+        int n = word.size();
+        unordered_map<char, int> vowels;
+        int consonantCount = 0;
+        long long result = 0;
+
+        // Precompute next consonant positions
+        vector<int> nextConsonant = createNextConsonant(word);
+        auto eraseVowel = [&](char c) {
+            if (--vowels[c] == 0) {
+                vowels.erase(c);
+            }
+        };
         // Sliding window
         int left = 0, right = 0;
         while (right < n) {
@@ -40,9 +50,7 @@ class Solution {
             // Shrink window if too many consonants
             while (left <= right && consonantCount > k) {
                 if (isVowel(word[left])) {
-                    if (--vowels[word[left]] == 0) {
-                        vowels.erase(word[left]);
-                    }
+                    eraseVowel(word[left]);
                 } else {
                     consonantCount--;
                 }
@@ -53,9 +61,7 @@ class Solution {
             while (left < right && vowels.size() == 5 && consonantCount == k) {
                 result += (nextConsonant[right] - right);
                 if (isVowel(word[left])) {
-                    if (--vowels[word[left]] == 0) {
-                        vowels.erase(word[left]);
-                    }
+                    eraseVowel(word[left]);
                 } else {
                     consonantCount--;
                 }
